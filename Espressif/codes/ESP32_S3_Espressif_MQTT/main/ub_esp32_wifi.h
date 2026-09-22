d442fef23db91234cc743bd5f1e2be0a7422dd21
jpbandera <jpbandera@uma.es>
2026-09-23 00:30:46 +0200

    ub WiFi and MQTT libraries created

---
+++ Espressif/codes/ESP32_S3_Espressif_MQTT/main/ub_esp32_wifi.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2026 Juan Pedro Bandera Rubio
+// Copyright (c) 2026 Grupo Avispa, DTE, Universidad de Málaga
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef UB_ESP32_WIFI_H
+#define UB_ESP32_WIFI_H
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+#include "nvs_flash.h"
+#include <time.h>
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <esp_log.h>
+#include <esp_sntp.h>
+#include <driver/gpio.h>
+
+void ub_esp32_wifi_init();
+
+#endif // UB_ESP32_WIFI_H
+
